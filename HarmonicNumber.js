6d3bfcd75184921d90e6b5326ce4c6e91c3f38d5
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-20 08:16:07 +0530

    Delete HarmonicNumber.js

--- HarmonicNumber.js
+++
@@ -1,6 +0,0 @@
-readline=require("readline-sync")
-let n =readline.question("Enter a number : " )
-let harmonic = "";
-for (let i = 1; i <= 5; i++) {
-  console.log(harmonic + "1/" + i + " or " + 1 / i);
-}
