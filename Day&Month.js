fffcc2bd18cbd3282f28ec988afd715aea15d2bf
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-20 08:24:48 +0530

    Delete Day&Month.js

--- Day&Month.js
+++
@@ -1,8 +0,0 @@
-readline = require("readline-sync")
-
-let day=readline.question("Enter the date :")
-let month=readline.question("Enter the month :")
-if (( (day <= 6 && month <= 20) && (month >=3 && day <= 20) && (day<31) ))
-console.log("True");
-else
-        console.log("False");
